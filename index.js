a2cc56808704bfa2d21964ea9fbaa90bda59f8bf
Benosn0714 <benson0714@gmail.com>
2022-01-25 17:53:04 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // 引用 line bot SDK
 let linebot = require('linebot');
+const express = require('express');
+const middleware = require('@line/bot-sdk').middleware;
+const JSONParseError = require('@line/bot-sdk').JSONParseError;
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed;
+
+const app = express();
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars
 let bot = linebot({
@@ -15,6 +21,7 @@ bot.on('message', function (event) {
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
-bot.listen('/', process.env.PORT || 5000, function () {
+bot.listen('/', process.env.PORT || 3000, function () {
   console.log('全國首家LINE線上機器人上線啦！！');
 });
+
