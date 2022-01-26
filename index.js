6f46ef50de31f4d35ddf65a817dedd570131d381
Benosn0714 <benson0714@gmail.com>
2022-01-27 01:46:48 +0800

    1

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-const koa = require("koa");
-const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
-const rq = require('request-promise');
-const logger = require('koa-logger')
-global.crypto = require('crypto');
-
-const app = new koa();
-const router = Router();
-
-app.use(bodyParser());
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+const router = new Router();
+const port = process.env.PORT || 4000;
 app.use(logger());
+app.use(bodyParser());
+
+app.on('error', (err, ctx) => {
+    console.log('server error', err, ctx)
+});
 
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 let lineBotToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
-const channelSecret = process.env.LINE_CHANNEL_SECRET;
+router
+    .get('/', (ctx, next) => {
+        console.log(ctx);
+        ctx.body = ctx;
+    })
+    .post('/webhook', async (ctx, next) => {
+        var reply_Token = ctx.request.body.events[0].replyToken;
+        console.log('token = ' , ctx.request.body.events[0].replyToken);
 
-router.post('/webhooks', async (ctx) => {
-  let events = ctx.request.body.events;
-    let responseText = (events, lineBotToken) => {
-      let message = events.message.text;
-      let replyToken = events.replyToken;
-      let options = {
-        method: 'POST',
-        uri: 'https://api.line.me/v2/bot/message/reply',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${lineBotToken}`,
+var rp_body = ({
+    replyToken: reply_Token,
+    messages: [{
+            type: 'text',
+            text: 'Hello'
         },
-        body: {
-            replyToken: replyToken,
-            messages: [{
-                type: "text",
-                text: message
-            }],
-        },
-        json: true
-    };
-    return(request(options));
-  }
-  data = await responseText(events, lineBotToken);
-  ctx.body = data;
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    });
+var options = {
+    method: 'POST',
+    url: 'https://api.line.me/v2/bot/message/reply',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${lineBotToken}`
+    },
+    json: true,
+    body: rp_body
+};
+var rp = require('request-promise');
+rp(options)
+    .then(function (parsedBody){
+        console.log('rq success');
+    })
+    .catch(function (err) {
+        console.log('server error', err, ctx);
+    });
 });
 
-app.use((ctx) => {
-  ctx.status = 200;
-})
-
-const server = app.listen(process.env.PORT || 3000, () => {
-  const port = server.address().port;
-  console.log("App now running on port", port);
-});
+app.listen(port);
+module.exports = { app }
