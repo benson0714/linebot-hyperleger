3c3e57925ba65e410075fe3abeb11826d8d1b20c
Benosn0714 <benson0714@gmail.com>
2022-01-26 23:33:25 +0800

    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
-const request = require('request-promise');
+const rp = require('request-promise');
 const logger = require('koa-logger')
 
 const app = new koa();
@@ -11,7 +11,18 @@ let lineBotToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 
 app.use(bodyParser());
 app.use(logger());
+
+app.on('error', (err, ctx) => {
+  console.log('server error', err, ctx)
+});
+
+app.use(router.allowedMethods());
+
 router
+.get('/', (ctx, next) => {
+  console.log(ctx);
+  ctx.body = ctx;
+})
   .post('/webhooks', async (ctx, next) => {
     let reply_token = ctx.request.body.events[0].replyToken;
     console.log('token = ', ctx.request.body.events[0].replyToken);
@@ -39,13 +50,13 @@ let options = {
   body: rp_body
 };
 
-request(options)
+rp(options)
   .then((parsedBody) => {
     console.log('rp sucess');
   })
   .catch((err) => {
     console.log('server error', err, ctx);
-  })
+  });
 });
 
 app.use(router.routes());
