507b9547b451c0f86ac1b4fcb7e292c65cc0719c
Benosn0714 <benson0714@gmail.com>
2022-01-26 20:59:08 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // 引用 line bot SDK
 let linebot = require('linebot');
+const koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+
+const app = new koa();
+const router = Router();
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
 let bot = linebot({
@@ -8,10 +14,18 @@ let bot = linebot({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
+
 let msg;
-let data;
 let userData;
 let dataAry = new Array();
+
+app.use(bodyParser());
+
+router.post('/webhooks', async (ctx, next) => {
+
+});
+
+app.use(router.routers);
 
 // 當有人傳送訊息給 Bot 時
 bot.on('message', function (event) {
@@ -26,11 +40,12 @@ bot.on('message', function (event) {
   if(msg === "hi" || msg === "Hi") {
     event.reply({type:"text", text:"hello"});
   } else {
-    event.reply({type:"text", text:`${dataAry[0]}+${dataAry[1]}`})
+    event.reply({type:"text", text:`+u`})
   }
   });
 
-// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
-bot.listen('/', process.env.PORT || 5000, function () {
-  console.log('全國首家LINE線上機器人上線啦！！');
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  const port = server.address().port;
+  console.log("App now running on port", port);
 });
