ce5829ad89750f53041dcbde47dd1283122a2182
Benosn0714 <benson0714@gmail.com>
2022-01-28 01:03:58 +0800

    1

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const app = new koa();
 const router = Router();
+const richMenu = require('./lib/example/richMenu.js');
 
 const channelSecret = process.env.LINE_CHANNEL_SECRET;
 const lineBotToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+//use body parser to check ctx.request.body
 app.use(bodyParser());
 app.use(logger());
 app.use(async (ctx, next) => {
@@ -36,25 +38,38 @@ router
             text: 'How are you?'
         }]
     };
-let options = {
-    method: 'POST',
-    url: 'https://api.line.me/v2/bot/message/reply',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${lineBotToken}`
-    },
-    json: true,
-    body: rp_body
-};
-ctx.body = rp(options)
-.then((body) => {
-    console.log('sucess');
+  let options = {
+      method: 'POST',
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${lineBotToken}`
+      },
+      json: true,
+      body: rp_body
+  };
+  ctx.body = rp(options)
+  .then((body) => {
+      console.log('sucess');
+  })
+  .catch((err) => {
+      console.log('err');
+  });
+
 })
-.catch((err) => {
-    console.log('err');
+.get('/create', async (ctx) => {
+  //create rich menu
+  let createDefaultRichMenu = await richMenu.createRichMenu(lineBotToken);
+  let richMenuId = createDefaultRichMenu.richMenuId;
+  console.log(richMenuId);
+  // upload rich menu image
+  let uploadRichMenuImageData = await richMenu.uploadRichMenuImage(richMenuId, `coffee.jpg`, lineBotToken);
+  console.log(uploadRichMenuImageData);
+  // Set default rich menu
+  let setDefaultRichMenuData = await richMenu.setDefaultRichMenu(richMenuId, lineBotToken);
+  ctx.body = setDefaultRichMenuData;
 });
 
-});
 
 app.use(router.routes());  
 
