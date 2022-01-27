5466f32a51814ddddf203d0c591ab3d5e75c3378
Benosn0714 <benson0714@gmail.com>
2022-01-27 22:04:43 +0800

    1

--- index.js
+++ index.js
@@ -20,12 +20,8 @@ app.use(async (ctx, next) => {
 router
 .post('/', async(ctx) => {
 
-  let event = ctx.request.body;
+  let event = ctx.request.query;
   console.log(event);
-  let message = event[0].message.text;
-  let replyToken = event[0].replyToken;
-  console.log(replyToken);
-  console.log(message);
   let rp_body = {
     replyToken: replyToken,
     messages: [{
