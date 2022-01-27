5d8ae6165728600a0696c8abe0c7ce33d283e626
Benosn0714 <benson0714@gmail.com>
2022-01-27 21:01:10 +0800

    1

--- index.js
+++ index.js
@@ -8,50 +8,56 @@ const router = Router();
 const channelSecret = process.env.LINE_CHANNEL_SECRET;
 const lineBotToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 
-// 收到 User Request 時要發回給 User 的訊息放這裡
-router.post('/webhooks', function *(ctx, next) {
-  // User 給 Bot 的訊息是一個 Array
-  const userMessages = ctx.request.body.events;
-  
-  // 準備回覆給 User 的訊息
-  let replyMessages = [];
-  replyMessages = userMessages.map((currentReplyMessages, userMessage) => {
-    // 等下放我們要回給 User 的訊息，一次最多 5 則
-    let messages;
+const koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const app = new koa();
+const router = Router();
 
-    // User 傳來的訊息型態
-    if ( userMessage.type === 'message' ) {
-      messages = [
-        {type: 'text', text: '你成功了！我回你就是了'},
-        {type: 'image', originalContentUrl: 'http://i.imgur.com/xjTET8v.jpg', previewImageUrl: 'https://sdl-stickershop.line.naver.jp/stickershop/v1/product/1179553/iphone/main@2x.png'},
-      ];
-      
-    }
-    else {
-      messages = [{type: 'text', text: '收到囉！'}];
-    }
-    
-    // request 的 options
+const channelSecret = process.env.LINE_CHANNEL_SECRET;
+const lineBotToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+
+app.use(logger());
+
+router.post('/webhook', async(ctx) => {      
+    let name = ctx.query.name;     
+    let talk = ctx.query.talk;      
+    ctx.body = `name${name}talk${talk}`;
+    let lineBotToken = 'sjLgjafLSBVaw6piUzrf+7NT8iLHhU+O8qFxvmwN2FUs4h0+as3kcyTJMvwLQ4bfzMAkbNWt7iLDhIOfk2qhlq3iwDQlsdQH6WZa5k/WTIpVWzdIaO/wOpCCxe+6DYk4D5IB+srXWtd0IfhVJ6niWwdB04t89/1O/w1cDnyilFU=';
+    let rp_body = ({
+        messages: [{
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            }]
+        });
     let options = {
         method: 'POST',
-        uri: 'https://api.line.me/v2/bot/message/reply',
-        // 客制化 headers 內容，主要告訴 Line 收到的訊息格式和 Line Bot 的 Token
+        url: 'https://api.line.me/v2/bot/message/broadcast',
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${lineBotToken}`,
-        },
-        body: {
-          // 告訴 Line 這個訊息回覆給誰
-          replyToken: userMessage.replyToken,
-          messages: messages,
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${lineBotToken}`
         },
         json: true,
-      };
+        body: rp_body
+    };
+    rp(options)
+    .then((body) => {
+        console.log('sucess');
+    })
+    .catch((err) => {
+        console.log('err');
+    });
 
-    return request(options);
-  });
-  
-  // 訊息處理好就等待所有 Request 發送出去
-  yield Promise.all( replyMessages );
-  
-})
+});
+
+app.use(router.routes());  
+
+const server = app.listen(process.env.PORT || 8080, function () {
+  const port = server.address().port;
+  console.log("App now running on port", port);
+});
