2c4da22e9ffd16b625923217c6e257653d48021b
Benosn0714 <benson0714@gmail.com>
2022-01-29 02:53:56 +0800

    1

--- test_request.js
+++ test_request.js
@@ -1,13 +1,22 @@
+const response = require('koa/lib/response');
 const rp = require('request-promise');
 
 let lineBotToken = 'sjLgjafLSBVaw6piUzrf+7NT8iLHhU+O8qFxvmwN2FUs4h0+as3kcyTJMvwLQ4bfzMAkbNWt7iLDhIOfk2qhlq3iwDQlsdQH6WZa5k/WTIpVWzdIaO/wOpCCxe+6DYk4D5IB+srXWtd0IfhVJ6niWwdB04t89/1O/w1cDnyilFU='
+var _include_headers = function(body, response, resolveWithFullResponse) {
+    return {'headers': response.headers, 'data': body};
+  };
 let listRichMenu = (lineBotToken) => {
     let options = {
         uri: 'https://line-sdk-test.herokuapp.com/listRichMenu',
+        transform: _include_headers,
         headers: {
-            'Authorization': `Bearer ${lineBotToken}`
+            'Authorization': `Bearer ${lineBotToken}`,
         },
+
     };
-    console.log(rp(options).data);
+    rp(options)
+    .then((response) =>　{
+        console.log(response.data)
+    });
 }
 listRichMenu(lineBotToken);
