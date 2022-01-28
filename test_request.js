bdf71eb251024a8f8cfc41281e4da3ce6712b982
Benosn0714 <benson0714@gmail.com>
2022-01-29 01:57:46 +0800

    1

--- test_request.js
+++ test_request.js
@@ -3,7 +3,7 @@ const rp = require('request-promise');
 let lineBotToken = 'sjLgjafLSBVaw6piUzrf+7NT8iLHhU+O8qFxvmwN2FUs4h0+as3kcyTJMvwLQ4bfzMAkbNWt7iLDhIOfk2qhlq3iwDQlsdQH6WZa5k/WTIpVWzdIaO/wOpCCxe+6DYk4D5IB+srXWtd0IfhVJ6niWwdB04t89/1O/w1cDnyilFU='
 let listRichMenu = (lineBotToken) => {
     let options = {
-        uri: 'https://api.line.me/v2/bot/richmenu/list',
+        uri: 'https://line-sdk-test.herokuapp.com/listRichMenu',
         headers: {
             'Authorization': `Bearer ${lineBotToken}`
         },
