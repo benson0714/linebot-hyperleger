874aa39e126da41c157748169d098672668e7964
Benson0714 <benson0714@gmail.com>
2022-02-12 18:37:05 +0800

    test

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const router = Router();
 const richMenu = require('./lib/example/richMenu.js');
 const hyperledger_api = require('./lib/example/hyperledger_api.js');
 const serve = require('koa-static');
-const path = require('path')
-const replyFlexMessage = require('./lib/requestServer/replyFlexMessage.js')
-const replyPostback = require('./lib/replyRoot/replyPostMessage.js')
+const path = require('path');
+const replyFlexMessage = require('./lib/requestServer/replyFlexMessage.js');
+const replyPostback = require('./lib/replyRoot/replyPostMessage.js');
+const replyTemplateMessage = require('./lib/requestServer/replyTemplateMessage.js');
 app.use(serve(path.join(__dirname, '/public')));
 
 const channelSecret = process.env.LINE_CHANNEL_SECRET;
