965825dfc8575d9e6d0d9954200e3054bcc18124
Benson0714 <benson0714@gmail.com>
2022-02-15 19:22:13 +0800

    add some new feature

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ router
   })
   .get('/qrcode', async(ctx) => {
     console.log('enter qrcode')
-    ctx.body = app.use(serve(path.join(__dirname, '/liff/qrcode')));
+    ctx.body = serve(path.join(__dirname, '/liff/qrcode'));
   });
 
 app.use(router.routes());
