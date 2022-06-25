7a0a362119c2545ab8f20e375a87b09d543d1e24
benson <benson0714@gmail.com>
2022-06-25 17:00:49 +0800

    transfer start

--- liff/amount/amount.js
+++ liff/amount/amount.js
@@ -116,7 +116,7 @@ $(function() {
     $('#btn').on('click', function(e) {
       e.preventDefault();
       var formData = $('form').serializeArray();
-        formData.push({'name':"tokenId",'value':getAllUrlParams().tokenId});
+        formData.push({'name':"tokenId",'value':getAllUrlParams().age});
       $.ajax({
         url:'/check_amount',
         type : "POST",
