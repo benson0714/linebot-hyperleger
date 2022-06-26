d9cb209a8a322c3d0e740da0a0493d6c40427326
benson <benson0714@gmail.com>
2022-06-26 17:13:46 +0800

    transfer start

--- liff/qrcode/qrcode.js
+++ liff/qrcode/qrcode.js
@@ -1,3 +1,72 @@
+// 不能用大寫的網址!!!
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  
+    // we'll store the parameters here
+    var obj = {};
+  
+    // if query string exists
+    if (queryString) {
+  
+      // stuff after # is not part of query string, so get rid of it
+      queryString = queryString.split('#')[0];
+  
+      // split our query string into its component parts
+      var arr = queryString.split('&');
+  
+      for (var i = 0; i < arr.length; i++) {
+        // separate the keys and the values
+        var a = arr[i].split('=');
+  
+        // set parameter name and value (use 'true' if empty)
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+  
+        // (optional) keep case consistent
+        paramName = paramName.toLowerCase();
+        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+  
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+  
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+  
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // otherwise add the value to the end of the array
+            obj[key].push(paramValue);
+          }
+        } else {
+          // we're dealing with a string
+          if (!obj[paramName]) {
+            // if it doesn't exist, create property
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // if property does exist and it's a string, convert it to an array
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // otherwise add the property
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+  
+    return obj;
+  }
+var userId = "";
+const amount = getAllUrlParams().amount
+const tokenId = getAllUrlParams().tokenid;
+const old_time = getAllUrlParams().time;
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
     const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
@@ -46,6 +115,12 @@ function initializeLiff(myLiffId) {
         .init({
             liffId: myLiffId
         })
+        .then(()=>{
+            liff.getProfile()
+            .then((res)=>{
+                userId = res['userId'];
+            })
+        })
 }
 
 
@@ -73,7 +148,7 @@ function getData(qrcode_address){
     return $.ajax({
         url:'/check_address',
         type : "POST",
-        data : `qrcode_address=${qrcode_address}`,
+        data : `[{qrcode_address:${qrcode_address}}, amount:${amount}, time:${old_time}, userId:${userId}, tokenId:${tokenId}]`,
         dataType: "json",
     })
 }
