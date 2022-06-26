33ad8458a2467d99c3009639423d8578e5dacef3
benson <benson0714@gmail.com>
2022-06-26 20:57:24 +0800

    transfer start

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ router
   .post('/check_amount', async (ctx) => {
     const data = ctx.request.body;
     console.log(data);
-    console.log(`amount = ${data.amount}`);
+    console.log(`amount = ${data.input_amount}`);
     console.log(`test console.log`);
     const amount = data.input_amount;
     const tokenId = data.tokenId;
