cbf35a7f370160600936a96677f4a17e70b18d86
benson <benson0714@gmail.com>
2022-07-01 20:36:32 +0800

    DB

--- liff/amount/amount.js
+++ liff/amount/amount.js
@@ -1,27 +1,28 @@
 var userId = '';
-window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+window.onload = function () {
+  const useNodeJS = true;   // if you are not using a node server, set this value to false
+  const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
 
-    // DO NOT CHANGE THIS
-    let myLiffId = "";
+  // DO NOT CHANGE THIS
+  let myLiffId = "";
+  let jwtToken = "";
 
-    // if node is used, fetch the environment variable and pass it to the LIFF method
-    // otherwise, pass defaultLiffId
-    if (useNodeJS) {
-        fetch('/send-amount')
-            .then(function(reqResponse) {
-                return reqResponse.json();
-            })
-            .then(function(jsonResponse) {
-                myLiffId = jsonResponse.id;
-                initializeLiffOrDie(myLiffId);
-            })
-
-    } else {
-        myLiffId = defaultLiffId;
+  // if node is used, fetch the environment variable and pass it to the LIFF method
+  // otherwise, pass defaultLiffId
+  if (useNodeJS) {
+    fetch('/send-amount')
+      .then(function (reqResponse) {
+        return reqResponse.json();
+      })
+      .then(function (jsonResponse) {
+        myLiffId = jsonResponse.id;
         initializeLiffOrDie(myLiffId);
-    }
+      })
+
+  } else {
+    myLiffId = defaultLiffId;
+    initializeLiffOrDie(myLiffId);
+  }
 };
 
 /**
@@ -29,11 +30,11 @@ window.onload = function() {
 * @param {string} myLiffId The LIFF ID of the selected element
 */
 function initializeLiffOrDie(myLiffId) {
-    if (!myLiffId) {
-        alert('initializeLiff first');
-    } else {
-        initializeLiff(myLiffId);
-    }
+  if (!myLiffId) {
+    alert('initializeLiff first');
+  } else {
+    initializeLiff(myLiffId);
+  }
 }
 
 /**
@@ -41,123 +42,121 @@ function initializeLiffOrDie(myLiffId) {
 * @param {string} myLiffId The LIFF ID of the selected element
 */
 function initializeLiff(myLiffId) {
-    liff
-        .init({
-            liffId: myLiffId
+  liff
+    .init({
+      liffId: myLiffId
+    })
+    .then(() => {
+      liff.getProfile()
+        .then((res) => {
+          userId = res['userId'];
+          return userId;
         })
-        .then(()=>{
-            liff.getProfile()
-            .then((res)=>{
-                userId = res['userId'];
-                return userId;
-            })
-            .then((res)=>{
-              const message = {
-                "userId": res
-              }
-              $.ajax({
-                url:'/createDB',
-                type : "POST",
-                data : message,
-                dataType: "json",
-                success : function(data) 
-                {
-                    console.log(data);
-                },error: function(data) 
-                {
-                    console.log('無法送出');
-                }
-            })     
-            })
+        .then((res) => {
+          const message = {
+            "userId": res
+          }
+          $.ajax({
+            url: '/createDB',
+            type: "POST",
+            data: message,
+            dataType: "json",
+            success: function (data) {
+              jwtToken = data['jwtToken'];
+            }, error: function (data) {
+              console.log('無法送出');
+            }
+          })
         })
+    })
 }
 
 // 不能用大寫的網址!!!
 function getAllUrlParams(url) {
 
-    // get query string from url (optional) or window
-    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-  
-    // we'll store the parameters here
-    var obj = {};
-  
-    // if query string exists
-    if (queryString) {
-  
-      // stuff after # is not part of query string, so get rid of it
-      queryString = queryString.split('#')[0];
-  
-      // split our query string into its component parts
-      var arr = queryString.split('&');
-  
-      for (var i = 0; i < arr.length; i++) {
-        // separate the keys and the values
-        var a = arr[i].split('=');
-  
-        // set parameter name and value (use 'true' if empty)
-        var paramName = a[0];
-        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-  
-        // (optional) keep case consistent
-        paramName = paramName.toLowerCase();
-        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-  
-        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-        if (paramName.match(/\[(\d+)?\]$/)) {
-  
-          // create key if it doesn't exist
-          var key = paramName.replace(/\[(\d+)?\]/, '');
-          if (!obj[key]) obj[key] = [];
-  
-          // if it's an indexed array e.g. colors[2]
-          if (paramName.match(/\[\d+\]$/)) {
-            // get the index value and add the entry at the appropriate position
-            var index = /\[(\d+)\]/.exec(paramName)[1];
-            obj[key][index] = paramValue;
-          } else {
-            // otherwise add the value to the end of the array
-            obj[key].push(paramValue);
-          }
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
         } else {
-          // we're dealing with a string
-          if (!obj[paramName]) {
-            // if it doesn't exist, create property
-            obj[paramName] = paramValue;
-          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-            // if property does exist and it's a string, convert it to an array
-            obj[paramName] = [obj[paramName]];
-            obj[paramName].push(paramValue);
-          } else {
-            // otherwise add the property
-            obj[paramName].push(paramValue);
-          }
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
         }
       }
     }
-  
-    return obj;
   }
 
+  return obj;
+}
 
-$(function() {
-    $('#btn').on('click', function(e) {
-      e.preventDefault();
-      var formData = $('form').serializeArray();
-        formData.push({'name':"tokenId",'value':getAllUrlParams().tokenid});
-        formData.push({'name':'userId', 'value':userId});
-      $.ajax({
-        url:'/check_amount',
-        type : "POST",
-        data : formData,
-        dataType: "json",
-        success : function(data) 
-        {
-            liff.closeWindow();
-        },error: function(data) 
-        {
-            console.log('無法送出');
-        }
+
+$(function () {
+  $('#btn').on('click', function (e) {
+    e.preventDefault();
+    var formData = $('form').serializeArray();
+    formData.push({ 'name': "tokenId", 'value': getAllUrlParams().tokenid });
+    formData.push({ 'name': 'userId', 'value': userId });
+    formData.push({"name":"jwtToken", "value": jwtToken});
+    
+    $.ajax({
+      url: '/check_amount',
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      success: function (data) {
+        liff.closeWindow();
+      }, error: function (data) {
+        console.log('無法送出');
+      }
     })
-    });
+  });
 });
 
