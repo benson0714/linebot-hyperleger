bc7543bd15f499afaf7041dd2a4c5b26b6f2829d
benson <benson0714@gmail.com>
2022-07-03 16:36:05 +0800

    DB

--- liff/amount/amount.js
+++ liff/amount/amount.js
@@ -70,30 +70,31 @@ function initializeLiff(myLiffId) {
             }
           })
         })
-        .then(()=>{
-          // 如果已經在step2狀態卻跑回來執行step1
-          state = 'step2handle'
-          if (state === 'step2handle') {
-            const message = {
-              "userId": res,
-              "state": state,
-              "currentState": "step1"
-            }
-            $.ajax({
-              url: '/errorStateHandle',
-              type: "POST",
-              data: message,
-              dataType: "json",
-              success: function (data) {
-                liff.closeWindow();
-              }, error: function (data) {
-                console.log('無法送出');
-              }
-            })
-          }else {
-            return;
+
+    })
+    .then(()=>{
+      // 如果已經在step2狀態卻跑回來執行step1
+      state = 'step2handle'
+      if (state === 'step2handle') {
+        const message = {
+          "userId": res,
+          "state": state,
+          "currentState": "step1"
+        }
+        $.ajax({
+          url: '/errorStateHandle',
+          type: "POST",
+          data: message,
+          dataType: "json",
+          success: function (data) {
+            liff.closeWindow();
+          }, error: function (data) {
+            console.log('無法送出');
           }
         })
+      }else {
+        return;
+      }
     })
 }
 
