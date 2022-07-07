d7d4faaa3bbcb42b7c5c96651758fde007190549
benson <benson0714@gmail.com>
2022-07-07 12:56:32 +0800

    DB

--- liff/amount/amount.js
+++ liff/amount/amount.js
@@ -204,6 +204,6 @@ $(function () {
 });
 
 $(window).load(function() { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-  $("#status").delay(5000).fadeOut(3000); //delay --> 延遲幾秒才fadeOut
-  $("#preloader").delay(7000).fadeOut(3000);
+  $("#status").delay(500).fadeOut(300); //delay --> 延遲幾秒才fadeOut
+  $("#preloader").delay(700).fadeOut(300);
 })
