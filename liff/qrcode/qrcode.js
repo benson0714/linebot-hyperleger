5cfe89171b9d9586f6a1e309e572806e227045e1
Benson0714 <benson0714@gmail.com>
2022-07-26 20:51:00 +0800

    carousel

--- liff/qrcode/qrcode.js
+++ liff/qrcode/qrcode.js
@@ -258,7 +258,7 @@ function getData(qrcode_address, userId) {
                   },
                   {
                     "type": "text",
-                    "text": "交易對象\n0x1af57c8dda4503843d043b96a12d7e6fa619c219",
+                    "text": "交易對象0x1af57c8dda4503843d043b96a12d7e6fa619c219",
                     "wrap": true
                   }
                 ]
@@ -288,7 +288,7 @@ function getData(qrcode_address, userId) {
           .sendMessages(temp)
           .then(() => {
             console.log("message sent");
-            // liff.closeWindow();
+            liff.closeWindow();
           })
           .catch((err) => {
             console.log("error", err);
