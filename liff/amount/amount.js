880a4fa0c60107c1061e0ff7cd73bd896322cddd
Benson0714 <benson0714@gmail.com>
2022-07-26 18:16:22 +0800

    carousel

--- liff/amount/amount.js
+++ liff/amount/amount.js
@@ -263,6 +263,6 @@ $(function () {
 });
 
 $(window).load(function () { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-  $("#status").delay(500).fadeOut(600); //delay --> 延遲幾秒才fadeOut
-  $("#preloader").delay(700).fadeOut(300);
+  $("#status").delay(800).fadeOut(300); //delay --> 延遲幾秒才fadeOut
+  $("#preloader").delay(900).fadeOut(300);
 })
