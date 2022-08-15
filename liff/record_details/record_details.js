27a7ae91944b819893e1d2b276a6397ad3c1f019
Benosn0714 <benson0714@gmail.com>
2022-08-15 19:08:54 +0800

    liffid

--- liff/record_details/record_details.js
+++ liff/record_details/record_details.js
@@ -139,10 +139,10 @@ function initializeLiff(myLiffId) {
               console.log(i)
               console.log(typeof(i))
                html_string = html_string + `<tr>
-               <td data-th="轉出/入">${data[i]["transfer_option"]}</td>
-               <td data-th="對象">${data[i]["transfer_address"]}</td>
-               <td data-th="數量">${data[i]["amount"]}</td>
-               <td data-th="時間">${data[i]["human_date"]}</td>
+               <td data-th="轉出/入">${record_array[i]["transfer_option"]}</td>
+               <td data-th="對象">${record_array[i]["transfer_address"]}</td>
+               <td data-th="數量">${record_array[i]["amount"]}</td>
+               <td data-th="時間">${record_array[i]["human_date"]}</td>
              </tr>`
             }
             console.log(html_string)
